bf4ef008e20eb67c10d21340e8ef83b18aab9e42
iamkarthikram <59197955+iamKarthikRam@users.noreply.github.com>
2024-01-31 09:59:01 +0000

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
 import "./styles.css";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FromFields = {
+  email: string;
+  password: string;
+};
+
 export default function App() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FromFields>({
+    defaultValues: {
+      email: "test@test.com",
+    },
+  });
+  const onSubmit: SubmitHandler<FromFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      throw new Error();
+    } catch (error) {
+      setError("email", { message: "This email is already taken" });
+      //setError("root", { message: "This email is already taken" }); //To display error for form
+    }
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("email", {
+            required: "Email is required",
+            validate: (value) => {
+              if (!value.includes("@")) {
+                return "Email must include @";
+              }
+              return true;
+            },
+          })}
+          type="text"
+        />
+        {errors.email && <div>{errors.email.message}</div>}
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must have at least 8 characters",
+            },
+          })}
+          type="password"
+        />
+        {errors.password && <div>{errors.password.message}</div>}
+        <button type="submit">Submit</button>
+        {/* {errors.root && <div>{errors.root.message}</div>}  for root error display */}
+      </form>
     </div>
   );
 }
